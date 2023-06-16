5666b7ff520c7ec60f073d4b6a44f0cc0af9ae19
Dongwon, Kim <tuna831@gmail.com>
2023-06-16 16:51:34 +0900

    deadrock test
    test deadrock test

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,44 +1,95 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace ServerCore
 {
+    class SessionManager
+    {
+        static object _lock = new object();
+
+        public static void TestSession()
+        {
+            lock (_lock)
+            {
+
+            }
+        }
+
+        public static void Test()
+        {
+            lock(_lock)
+            {
+                UserManager.TestUser();
+            }
+        }
+    }
+
+    class UserManager
+    {
+        static object _lock = new object();
+
+        public static void Test()
+        {
+
+            lock (_lock)
+            {
+                SessionManager.TestSession();
+            }
+        }
+
+        public static void TestUser()
+        {
+            lock( _lock)
+            {
+                
+            }    
+        } 
+    }
 
     class Program
     {
-        volatile static bool _stop = false;
-       
-        static void ThreadMain()
+        static int number = 0;
+        static object _obj = new object();
+        static object _obj2 = new object();
+
+        //atomic 원자성 
+        /**
+         * 
+         * 
+         */
+        static void Thread_1()
         {
-            Console.WriteLine("쓰레드 시작");
-
-            while(_stop == false)
+            for (int i = 0; i < 10000; i++)
             {
-                //someone want to be stop
+                SessionManager.Test();
+            }    
+        }
+        
+        static void Thread_2()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                UserManager.Test();
             }
-
-            Console.WriteLine("finish thread");
-             
 
         }
 
+        // Deadlock lock을 안풀고 끝냈을때 
         static void Main(string[] args)
         {
-            Task t = new Task(ThreadMain);
-            t.Start();
 
-            Thread.Sleep(1000);
+            Task t1 = new Task(Thread_1);  
+            Task t2 = new Task(Thread_2);
+            t2.Start();
+            t1.Start();
 
-            _stop = true;
 
-            Console.WriteLine("call stop");
-            Console.WriteLine("wait finish");
-            t.Wait();
+            Task.WaitAll(t1, t2);
 
-            Console.WriteLine("finish success");
-            
+            Console.WriteLine(number);
         }
     }
 }
